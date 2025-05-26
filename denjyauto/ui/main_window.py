991edb992ca8bff38f139f7a91f1142ac1bff5b7
rosica.msp <internet2222@abv.bg>
2025-05-26 10:27:08 +0300

    style general win

--- denjyauto/ui/main_window.py
+++ denjyauto/ui/main_window.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter import ttk
 from denjyauto.ui.widgets import create_scrollable_frame
 
@@ -10,22 +11,23 @@ class MainWindow:
         self.master = master
         master.title("DenjyAuto")
 
-        self.header = ttk.Label(master, text="D E N J Y  A U T O", font=("Arial", 16), foreground="red")
-        self.header.pack(pady=10)
+        self.logo_image = tk.PhotoImage(file="images/denjyauto.gif")
 
-        self.toolbar = ttk.Frame(master)
-        self.toolbar.pack(pady=5)
+        self.start_frame = ttk.Frame(master)
+        self.start_frame.pack(side="top", anchor="nw", fill="y", pady=5)
+
+        self.logo = ttk.Label(self.start_frame, image=self.logo_image, font=("Arial", 16), foreground="red")
+        self.logo.pack(side="left", pady=10, padx=50)
+
+        self.refresh_button = ttk.Button(self.start_frame, text="    ОБНОВИ СПИСЪКА    ", command=lambda: load_clients(master, self.content_frame))
+        self.refresh_button.pack(side="left", pady=10, padx=50)
+
+        self.new_client_button = ttk.Button(self.start_frame, text="    НОВ КЛИЕНТ    ", command=lambda: open_new_client_form(master))
+        self.new_client_button.pack(side="left", pady=10, padx=50)
+
+        self.profit_button = ttk.Button(self.start_frame, text="    ПРИХОД    ", command=lambda: income(master))
+        self.profit_button.pack(side="left", pady=10, padx=50)
 
         self.content_frame = create_scrollable_frame(master, scroll="both")
-
-        self.refresh_button = ttk.Button(self.toolbar, text="Обнови списъка", command=lambda: load_clients(master, self.content_frame))
-        self.refresh_button.grid(row=0, column=0, padx=5)
-
-        self.new_client_button = ttk.Button(self.toolbar, text="Нов клиент", command=lambda: open_new_client_form(master))
-        self.new_client_button.grid(row=0, column=1, padx=5)
-
-        self.profit_button = ttk.Button(self.toolbar, text="Приход", command=lambda: income(master))
-        self.profit_button.grid(row=0, column=2, padx=5)
-
 
         load_clients(master, self.content_frame)
